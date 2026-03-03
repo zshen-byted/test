7e6d05e492c8083622cbdc6ebc9417b8a1f15b35
ssz9 <shenzt23@mails.tsinghua.edu.cn>
2026-03-04 05:34:55 +0800

    impl TorchFunctionMode align instrumentation.

--- alignment/alignment.py
+++ alignment/alignment.py
@@ -1,3 +1,5 @@
+import functools
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,7 +18,7 @@ import collections
 from typing import Optional, Tuple, List, NamedTuple
 
 
-dynamo.config.error_on_recompile = True
+# dynamo.config.error_on_recompile = True
 
 class AlignmentNode:
     def __init__(self, id:int):
@@ -27,7 +29,7 @@ class AlignmentNode:
 
 class AlignmentManager:
     _instance: Optional["AlignmentManager"] = None
-    _nodes: dict[int, dict] = {}
+    _nodes: dict[int, AlignmentNode] = {}
 
     def __new__(cls):
         if cls._instance is None:
@@ -94,19 +96,21 @@ class AlignmentManager:
     @classmethod
     def print_data_comparison(cls, model_ids: list[str], gold_model_id: Optional[str] = None):
         gold_model_id = gold_model_id if gold_model_id is not None else model_ids[0]
-        for node_id, align_node in sorted(cls._nodes.items()):
-            print(f"\nAlignmentNode {node_id}:")
-            gold_data = align_node.data.get(gold_model_id, [])
-            if not gold_data:
-                print(f"  Gold model {gold_model_id} has no data collected for this node.")
-                continue
-            for model_id in model_ids:
-                data = align_node.data.get(model_id, [])
-                if not data:
-                    print(f"  {model_id}: No data collected")
+        for i in range(list(cls._nodes.values())[0].data.__len__()):
+            print(f"\n{'='*40}\nStep {i}\n{'='*40}")
+            for node_id, align_node in sorted(cls._nodes.items()):
+                print(f"AlignmentNode {node_id}:")
+                gold_data = align_node.data.get(gold_model_id, [])
+                if not gold_data:
+                    print(f"  Gold model {gold_model_id} has no data collected for this node.")
                     continue
-                print(f"  {model_id}{' (gold)' if model_id == gold_model_id else '':8}:", end="")
-                for i, (xorsum, raw_tensor) in enumerate(data):
+                for model_id in model_ids:
+                    data = align_node.data.get(model_id, [])
+                    if not data:
+                        print(f"  {model_id}: No data collected")
+                        continue
+                    print(f"  {model_id+(' (gold)' if model_id == gold_model_id else ''):15}:", end="")
+                    (xorsum, raw_tensor) = data[i]
                     gold_xorsum, gold_tensor = gold_data[i]
                     raw_tensor_32, gold_tensor_32 = raw_tensor.to(torch.float32), gold_tensor.to(torch.float32)
 
@@ -114,6 +118,7 @@ class AlignmentManager:
                     closeto:bool = torch.allclose(raw_tensor_32, gold_tensor_32, rtol=1e-3, atol=1e-5)
 
                     print(f"dtype={raw_tensor.dtype}, xorsum=0x{xorsum:08X}, max_diff={max_diff:.8f}, closeto={str(closeto):5}")
+                print("")
 
     @staticmethod
     def xorsum32(t: torch.Tensor) -> int:
@@ -124,92 +129,123 @@ class AlignmentManager:
         return int(np.bitwise_xor.reduce(u32.numpy(), dtype=np.uint64)) & 0xFFFFFFFF
 
 
+    @staticmethod
+    def inject_marker_meta_and_remove_marker_pass(gm: torch.fx.GraphModule) -> torch.fx.GraphModule:
+        graph = gm.graph
+        markers_to_remove = []
 
-def inject_marker_meta_and_remove_marker_pass(gm: torch.fx.GraphModule) -> torch.fx.GraphModule:
-    graph = gm.graph
-    markers_to_remove = []
+        for node in graph.nodes:
+            if node.op == "call_function" and node.target == torch.ops.xpugraph.marker.default:
+                # args = (input_tensor, node_id)
+                source_node: torch.fx.Node = node.args[0]
+                node_id: int = node.args[1]
 
-    for node in graph.nodes:
-        if node.op == "call_function" and node.target == torch.ops.xpugraph.marker.default:
-            # args = (input_tensor, node_id)
-            source_node: torch.fx.Node = node.args[0]
-            node_id: int = node.args[1]
+                # 注入 alignment node_id 到被 mark 的算子的 meta 中
+                if "align_node_ids" not in source_node.meta:
+                    source_node.meta["align_node_ids"] = []
+                source_node.meta["align_node_ids"].append(node_id)
 
-            # 注入 alignment node_id 到被 mark 的算子的 meta 中
-            if "align_node_ids" not in source_node.meta:
-                source_node.meta["align_node_ids"] = []
-            source_node.meta["align_node_ids"].append(node_id)
+                # marker 的输出直接替换为其输入（bypass marker）
+                node.replace_all_uses_with(source_node)
+                markers_to_remove.append(node)
 
-            # marker 的输出直接替换为其输入（bypass marker）
-            node.replace_all_uses_with(source_node)
-            markers_to_remove.append(node)
+        for node in markers_to_remove:
+            graph.erase_node(node)
 
-    for node in markers_to_remove:
-        graph.erase_node(node)
+        graph.lint()
+        gm.recompile()
+        return gm
 
-    graph.lint()
-    gm.recompile()
-    return gm
+    @staticmethod
+    def insert_instrument_nodes_pass(gm: torch.fx.GraphModule, model_id: str) -> torch.fx.GraphModule:
+        graph = gm.graph
+
+        for node in list(graph.nodes):
+            align_node_ids = node.meta.get("align_node_ids", [])
+            for node_id in align_node_ids:
+                with graph.inserting_after(node):
+                    instrument_node = graph.call_function(
+                        torch.ops.xpugraph.instrument.default,
+                        args=(node, node_id, model_id),
+                    )
+
+        graph.lint()
+        gm.recompile()
+        return gm
 
 
-def insert_instrument_nodes_pass(gm: torch.fx.GraphModule, model_id: str) -> torch.fx.GraphModule:
-    graph = gm.graph
 
-    for node in list(graph.nodes):
-        align_node_ids = node.meta.get("align_node_ids", [])
-        for node_id in align_node_ids:
-            with graph.inserting_after(node):
-                instrument_node = graph.call_function(
-                    torch.ops.xpugraph.instrument.default,
-                    args=(node, node_id, model_id),
-                )
+class AlignedModelGenerator:
 
-    graph.lint()
-    gm.recompile()
-    return gm
-
-class AlignmentManagerContext(TorchFunctionMode):
-    def __init__(self, model_id: str, function_inject=False, module_hook_inject=False, module:Optional[nn.Module]=None):
-        super().__init__()
-        self.model_id = model_id
-        self.align_mgr = AlignmentManager()
-        self._next_node_id = 0
-        self.function_inject = function_inject
-        #TODO module_hook_inject
+    class MarkerInjectFuntionMode(TorchFunctionMode):
     
-    def __torch_function__(self, func, types, args=(), kwargs=None):
-        result = func(*args, **(kwargs or {}))
-        if self._should_inject_function_marker(func, types, result):
-            result = torch.ops.xpugraph.marker(result, self._new_node_id())
-        return result
+        def __init__(self, ctx: "AlignedModelGenerator"):
+            super().__init__()
+            self._ctx = ctx
+            self._stop_inject: bool = False
 
-    def _should_inject_function_marker(self, func, types, result) -> bool:
-        ret = self.function_inject
-        if not isinstance(result, torch.Tensor):
-            ret = False
+        def __torch_function__(self, func, types, args=(), kwargs=None):
+            result = func(*args, **(kwargs or {}))
+            if self._should_inject_function_marker(func, types, result):
+                result = torch.ops.xpugraph.marker(result, self._ctx._new_node_id())
+            return result
 
-        return ret
+        def _should_inject_function_marker(self, func, types, result) -> bool:
+            if self._stop_inject:
+                return False
+            if not isinstance(result, torch.Tensor):
+                return False
+            return True
+    
+    class EagerInstrumentFunctionMode(TorchFunctionMode):
+        
+        def __init__(self, ctx: "AlignedModelGenerator", model_id: str):
+            super().__init__()
+            self._ctx = ctx
+            self._model_id = model_id
+            self._next_node_id = 0
+
+        def __torch_function__(self, func, types, args=(), kwargs=None):
+            result = func(*args, **(kwargs or {}))
+            
+            if isinstance(result, torch.Tensor):
+                node_id = self._next_node_id
+                self._next_node_id += 1
+                # call instrument op directly to collect data in eager mode
+                torch.ops.xpugraph.instrument(result, node_id, self._model_id)
+                # print(f"Instrumenting function {func.__name__} with result type {type(result)}, node_id={node_id}")
+            return result
+
+    def __init__(self, model_id: str):
+        self.align_mgr = AlignmentManager()
+        self.model_id = model_id
+        self._next_node_id = 0
+        self._marker_inject_mode = self.MarkerInjectFuntionMode(self)
+        self._eager_instrument_mode = self.EagerInstrumentFunctionMode(self, model_id)
+        self._generated = False # one AlignedModelGenerator instance could only generate one compiled/eager model.
 
     def _new_node_id(self):
         ret = self._next_node_id
         self._next_node_id += 1
         return ret
 
-    def get_backend(self):
+    import functools
+
+    def _make_backend(self):
+
         def alignment_fw_compiler(gm: torch.fx.GraphModule, example_inputs):
             gm.graph.print_tabular()
-            
+
             print("", flush=True)
             print("\n=== Injecting marker meta and removing marker nodes ===", flush=True)
-            gm = inject_marker_meta_and_remove_marker_pass(gm)
+            gm = self.align_mgr.inject_marker_meta_and_remove_marker_pass(gm)
             gm.graph.print_tabular()
-            
+
             print("", flush=True)
             print("\n=== Inserting instrument nodes ===", flush=True)
-            gm = insert_instrument_nodes_pass(gm, self.model_id)
+            gm = self.align_mgr.insert_instrument_nodes_pass(gm, self.model_id)
             gm.graph.print_tabular()
             print("", flush=True)
-
 
             return gm.forward
 
@@ -217,7 +253,6 @@ class AlignmentManagerContext(TorchFunctionMode):
             return gm.forward
 
         def backend(gm: torch.fx.GraphModule, example_inputs):
-
             gm = aot_module_simplified(
                 gm,
                 example_inputs,
@@ -226,7 +261,7 @@ class AlignmentManagerContext(TorchFunctionMode):
             )
 
             tracing_context = torch._guards.TracingContext.try_get()
-            orig_guards:torch._guards.GuardsSet= tracing_context.guards_context.dynamo_guards
+            orig_guards: torch._guards.GuardsSet = tracing_context.guards_context.dynamo_guards
             filter_flags = [False for _ in orig_guards]
             filtered_guards = torch._guards.GuardsSet(
                 set(guard for guard, flag in zip(orig_guards, filter_flags) if not flag)
@@ -234,8 +269,50 @@ class AlignmentManagerContext(TorchFunctionMode):
             tracing_context.guards_context.dynamo_guards = orig_guards - filtered_guards
 
             return gm
-        return backend
 
+        return backend
+    
+    def get_compiled(self, model: nn.Module, example_inputs) -> nn.Module:
+        if self._generated:
+            raise RuntimeError("A AlignedModelGenerator instance can only generate once. Please create a new instance for another model.")
+
+        with self._marker_inject_mode:
+            compiled_fn = torch.compile(model, backend=self._make_backend(), dynamic=True, fullgraph=True)
+            _ = compiled_fn(*example_inputs) # warmup to trigger compilation and data collection
+        self._marker_inject_mode._stop_inject = True
+        # clear manager node data
+        for node in self.align_mgr._nodes.values():
+            node.data.clear()
+
+        class _WrapModuleCompile(nn.Module):
+            def __init__(wself, fn):
+                super().__init__()
+                wself.fn = fn
+
+            @dynamo.disable
+            def forward(wself, *args, **kwargs):
+                # this 'with' is for hacking dynamo torch_function_stack checker guard.
+                with self._marker_inject_mode:
+                    return wself.fn(*args, **kwargs)
+                
+        self._generated = True
+        return _WrapModuleCompile(compiled_fn)
+
+    def get_eager(self, model: nn.Module) -> nn.Module:
+        if self._generated:
+            raise RuntimeError("A AlignedModelGenerator instance can only generate once. Please create a new instance for another model.")
+        class _WrapModule(nn.Module):
+            def __init__(wself, model):
+                super().__init__()
+                wself._model = model
+
+            def forward(wself, *args, **kwargs):
+                self._eager_instrument_mode._next_node_id = 0 # reset node_id counter before each forward
+                with self._eager_instrument_mode:
+                    return wself._model(*args, **kwargs)
+
+        self._generated = True
+        return _WrapModule(model)
 
 class MyModel(nn.Module):
     def __init__(self):
@@ -255,50 +332,31 @@ if __name__ == "__main__":
     
     mgr = AlignmentManager()
 
-    # Model A: 原始 float32
+    # Model A: eager float32
     with torch.random.fork_rng():
-        model_a = MyModel()
-        input = torch.randn(4, 16)
-        with AlignmentManagerContext("model_A", function_inject=True) as am_ctx:
-            compiled_a = torch.compile(model_a, backend=am_ctx.get_backend(), dynamic=True, fullgraph=True, )
-            _ = compiled_a(input)
+        model_a = AlignedModelGenerator("eager_A").get_eager(MyModel())
+
+    # Model A: compiled float32
+    with torch.random.fork_rng():
+        compiled_a = AlignedModelGenerator("compiled_A").get_compiled(MyModel(), (torch.randn(4, 16),))
+
+    # Model B: eager float16
+    with torch.random.fork_rng():
+        model_b = AlignedModelGenerator("eager_B").get_eager(MyModel().half())
+    
+    # Model B: compiled float16
+    with torch.random.fork_rng():
+        compiled_b = AlignedModelGenerator("compiled_B").get_compiled(MyModel().half(), (torch.randn(4, 16).half(),))
 
 
-    print("\n=== Running inference to collect data ===", flush=True)
+    for i in range(10):
+        x = torch.randn(4, 16)
 
-    x = torch.randn(4, 16)
-
-    y_a = compiled_a(x.clone())
-
-
-    # # Model B: 同样的模型
-    # with torch.random.fork_rng():
-    #     model_b = MyModel()
-    #     input = torch.randn(4, 16)
-    #     with AlignmentManagerContext("model_B", function_inject=True) as am_ctx:
-    #         compiled_b = torch.compile(model_b, backend=am_ctx.get_backend(), dynamic=True, fullgraph=True)
-    #         _ = compiled_b(input)
-
-    # # Model C: 量化到float16
-    # with torch.random.fork_rng():
-    #     model_c = MyModel().half()
-    #     input = torch.randn(4, 16).half()
-    #     with AlignmentManagerContext("model_C", function_inject=True) as am_ctx:
-    #         compiled_c = torch.compile(model_c, backend=am_ctx.get_backend(), dynamic=True, fullgraph=True)
-    #         _ = compiled_c(input)
-
-    print("\n=== Running inference to collect data ===", flush=True)
-
-    x = torch.randn(4, 16)
-
-    y_a = compiled_a(x.clone())
-
-    # y_b = compiled_b(x.clone())
-
-    # y_c = compiled_c(x.clone().half())
+        _ = model_a(x.clone())
+        _ = compiled_a(x.clone())
+        _ = model_b(x.clone().half())
+        _ = compiled_b(x.clone().half())
 
 
-    # # ----  对比所有model 在每个 AlignmentNode 上的记录 ----
-    # print(f"\n{'='*60}\n对齐结果对比:\n{'='*60}")
-
-    # mgr.print_data_comparison(["model_A", "model_B", "model_C"])
+    print(f"\n{'='*60}\n对齐结果对比:\n{'='*60}")
+    mgr.print_data_comparison(["eager_A", "compiled_A", "eager_B", "compiled_B"], gold_model_id="eager_A")
